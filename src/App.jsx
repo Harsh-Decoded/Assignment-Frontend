93b1390bf5039bb6dc159a10e5142e5e882ef9d4
Harsh-Decoded <gamerharsh0@gmail.com>
2024-07-12 12:42:43 +0530

    Deploment changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,18 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Dashboard from "@/components/Dashboard";
 import SignUpForm from "@/components/Signup";
-import {
-  Route,
-  Routes,
-  BrowserRouter
-} from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-      <BrowserRouter>
-        <ToastContainer />
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/signup" element={<SignUpForm />} />
-        </Routes>
-      </BrowserRouter>
-  )
+    <BrowserRouter>
+      <ToastContainer />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/signup" element={<SignUpForm />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
